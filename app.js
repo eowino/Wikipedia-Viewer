36a1bd2a9ba42dfd91082144af6563a5818856de
Evans Owino <e.owino@replyltd.co.uk>
2016-07-29 14:07:31 +0100

    Progress 1

---
+++ app.js
@@ -0,0 +1,20 @@
+//https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=sony&callback=angular.callbacks._0
+
+var URLHead = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+var userSearch;
+var URLTail = "&callback=angular.callbacks._0";
+
+
+
+
+/*
+
+WRAP THE LIST ITEMS IN ANCHOR TAGS 
+
+******* DISPLAY TWO IN MEDIUM SIZED SCREENS AND ONE TILE ON MOBILE ******* ?
+
+
+
+*/
+
+
